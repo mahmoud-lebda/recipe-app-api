cce05fd104a430d424362fbe8a5ed0d9c7bd9645
Mahmoud Lebda <mahmoud.lebda@gmail.com>
2022-10-22 04:41:11 +0200

    fix error

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -27,12 +27,12 @@ class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email = serializers.EmailField()
     password = serializers.CharField(
-        Style={'input_type': 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False,
     )
 
     def validate(self, attrs):
-        """Validate and authenticate """
+        """Validate and authenticate the user."""
         email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
