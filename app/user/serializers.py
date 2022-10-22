8a7a9add755c860d35bbbbdcf6a3661566f3983a
Mahmoud Lebda <mahmoud.lebda@gmail.com>
2022-10-22 05:10:10 +0200

    add update loggend in user

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,7 +4,7 @@ Serializers for the user API View.
 from django.contrib.auth import (
     get_user_model,
     authenticate,
-    )
+)
 
 from django.utils.translation import gettext as _
 
@@ -21,6 +21,16 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
